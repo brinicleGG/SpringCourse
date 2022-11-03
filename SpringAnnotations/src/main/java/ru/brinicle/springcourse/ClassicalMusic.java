8deca7e351fffd662f0410994ac982ad1c7e2bfb
brinicleGG <GG9DJM@GMAIL.COM>
2022-11-03 16:11:45 +0300

    Урок №12

--- SpringAnnotations/src/main/java/ru/brinicle/springcourse/ClassicalMusic.java
+++ SpringAnnotations/src/main/java/ru/brinicle/springcourse/ClassicalMusic.java
@@ -2,8 +2,21 @@ package ru.brinicle.springcourse;
 
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 public class ClassicalMusic implements Music {
+
+    @PostConstruct
+    public void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+
+    @PreDestroy
+    public void doMyDestroy() {
+        System.out.println("Doing my destruction");
+    }
 
     @Override
     public String getSong() {
--- SpringAnnotations/src/main/java/ru/brinicle/springcourse/MusicPlayer.java
+++ SpringAnnotations/src/main/java/ru/brinicle/springcourse/MusicPlayer.java
@@ -2,10 +2,17 @@ package ru.brinicle.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
+
+    @Value("${musicPlayer.name}")
+    private String name;
+
+    @Value("${musicPlayer.volume}")
+    private int volume;
 
     private Music music1;
     private Music music2;
@@ -16,6 +23,14 @@ public class MusicPlayer {
 
         this.music1 = music1;
         this.music2 = music2;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
     }
 
     public String playMusic() {
